1562f61944c9242bd6001956ae262498e0ca2756
Fabio Oliveira <oliveirafabiopereirade@gmail.com>
2020-06-22 11:12:49 -0300

    Obtencao de data com Date() e selecao de seu componentes com .getxxx()

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-var count;
-for (count = 0; count <= 5; count++){
-    console.log(count);
-    alert(count);
-}
+var d = new Date();
+console.log(d);
+console.log("Dia da semana " + d.getDay());
+console.log("Dia do mês " + d.getUTCDate());
+console.log("Mês " + (d.getMonth() + 1)); // soma 1 porque começa a contar do 0 (janeiro)
+console.log(d.getUTCFullYear());
+console.log(d.getHours());
+console.log(d.getMinutes());
+console.log(d.getSeconds());
+
