476fec6600dfe3178d2d249a0228df5b1d26a0e2
Fabio Oliveira <oliveirafabiopereirade@gmail.com>
2020-06-22 10:54:49 -0300

    Implementando laço de repeticao while

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-var idade = prompt("Qual a sua idade ?");
-if (idade >= 18){
-    alert("Maior de idade.");
-}else{
-    alert("Menor de idade.");
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+//    count = count + 1;
+    count++;
 }
