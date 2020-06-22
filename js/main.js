e89b27f84d66a30260660327dc2b16bbf8772dc2
Fabio Oliveira <oliveirafabiopereirade@gmail.com>
2020-06-22 09:46:35 -0300

    Uso da função replace em strings

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 var nome = "Fabio Oliveira";
 var idade = 43;
 var decada = 10;
+var frase = "O Japão é o melhor time do mundo"
 /*alert(nome + " tem " + idade + " anos");
 alert(idade + decada);*/
 console.log(nome);
 console.log(idade + decada);
+console.log(frase.replace("Japão", "Brasil"));
+alert(frase.replace("Japão", "Brasil"));
