b76607a9d85f8ae9741bf5c2da64317fb8420e56
Fabio Oliveira <oliveirafabiopereirade@gmail.com>
2020-06-22 12:45:43 -0300

    Utiliza eventos mouseover e mouseout para alterar HTML, e utiliza (this) para identificar elementos nas funcoes

--- js/main.js
+++ js/main.js
@@ -6,3 +6,13 @@ function redirecionar(){
 //    window.open("https://digitalinnovation.one/");            // abre página em outra janela
     window.location.href = "https://digitalinnovation.one/";  // abre página na mesma janela
 }
+
+function trocar(elemento){ // recebe id do elemento como argumento
+//    document.getElementById("mousemove").innerHTML = "<b>Obrigado por passar o mouse!</b>"; // usa o id do elemento
+    elemento.innerHTML = "<b>Obrigado por passar o mouse!</b>";                             // usa o id recebido
+}
+
+function voltar(elemento){ // recebe elemento como argumento                                // usa o id do elemento
+//    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";                  // usa o id recebido
+    elemento.innerHTML = "Passe o mouse aqui";
+}
