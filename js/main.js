c284b4c8f7fe7e7a0470a89ebb23ccbb34de4ba6
Fabio Oliveira <oliveirafabiopereirade@gmail.com>
2020-06-22 10:48:34 -0300

    Captura de dados do usuario com prompt() e decisao com if / else

--- js/main.js
+++ js/main.js
@@ -1,47 +1,6 @@
-var fruta = {nome:"maçã", cor:"vermelha"};
-console.log(fruta.nome);
-alert(fruta.cor);
-
-var frutas =    [
-                    {nome:"maçã", cor:"vermelha"},
-                    {nome:"uva", cor:"roxa"},
-                    {nome:"laranja", cor:"laranja"},
-                ];
-console.log(frutas);
-console.log(frutas[1].nome);
-alert(frutas[0].cor);
-
-
-
-/*
-var lista = ["maçã", "pêra", "laranja"];
-lista.push("uva");
-console.log(lista);
-//console.log(lista.length);
-//lista.pop();
-//console.log(lista);
-//console.log(lista.length);
-//console.log(lista.reverse());
-//console.log(lista.toString());
-//console.log(lista[0]);
-//console.log(lista.toString()[0]);
-console.log(lista.join(" - "));
-console.log(lista.join(" | "));
-console.log(lista.join("   "));
-console.log(lista.join(""));
-*/
-
-
-/*
-var nome = "Fabio Oliveira";
-var idade = 43;
-var decada = 10;
-var frase = "O Japão é o melhor time do mundo"
-//alert(nome + " tem " + idade + " anos");
-//alert(idade + decada);
-console.log(nome);
-console.log(idade + decada);
-console.log(frase.toUpperCase());
-//alert(frase.replace("Japão", "Brasil"));
-console.log(frase.toLowerCase());
-*/
+var idade = prompt("Qual a sua idade ?");
+if (idade >= 18){
+    alert("Maior de idade.");
+}else{
+    alert("Menor de idade.");
+}
