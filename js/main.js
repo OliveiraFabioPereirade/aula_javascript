94e4255d24972cc45b73cddc8184ad8faf0b7190
Fabio Oliveira <oliveirafabiopereirade@gmail.com>
2020-06-22 09:37:16 -0300

    Utilizando o console do navegador para exibir mensagens

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var nome = "Fabio Oliveira";
 var idade = 43;
 var decada = 10;
-//alert(nome + " tem " + idade + " anos");
-alert(idade + decada);
+/*alert(nome + " tem " + idade + " anos");
+alert(idade + decada);*/
+console.log(nome);
+console.log(idade + decada);
