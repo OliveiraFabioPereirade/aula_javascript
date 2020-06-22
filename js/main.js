256724358c8b8b8d866fa9e8a049444ebb380e5f
Fabio Oliveira <oliveirafabiopereirade@gmail.com>
2020-06-22 12:01:10 -0300

    Chamando funcoes atraves de botoes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
-var validar = 0;   // aqui "validar" é uma variável global
-
-function validaIdade(idade){
-//    var validar; // aqui "validar" é uma variável local
-    if (idade >=18){
-        validar = true;
-    }else{
-        validar = false;
-    }
+function clicou(){
+    alert("Obrigado por clicar!");
 }
 
-var idade = prompt("Qual a sua idade ?")
-validaIdade(idade);
-console.log(validar); // só funciona quando "validar" é uma variável global
