9e23fbe2904e55b47161d093fe17332f035a7e88
Fabio Oliveira <oliveirafabiopereirade@gmail.com>
2020-06-22 12:19:24 -0300

    Abre outra página em outra janela (window.open()) ou na mesma janela (window.location.href)

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ function clicou(){
     document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar!</b>"; // <b> deixa o texto bold
 }
 
+function redirecionar(){
+//    window.open("https://digitalinnovation.one/");            // abre página em outra janela
+    window.location.href = "https://digitalinnovation.one/";  // abre página na mesma janela
+}
